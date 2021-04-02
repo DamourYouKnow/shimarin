291e6dc1ad59eca0ceeb1c2b644be3f3fd91ba57
damouryouknow <bailey@damour.xyz>
2021-04-02 00:31:04 -0400

    Add updates command

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import * as Bot from './bot';
 import UtilityModule from './modules/utility';
 import SearchModule from './modules/search';
 import ListModule from './modules/list';
+import UpdatesModule from './modules/updates';
 
 const bot = new Bot.Bot(new Discord.Client());
 
 bot.addModule(new UtilityModule(bot));
 bot.addModule(new SearchModule(bot));
 bot.addModule(new ListModule(bot));
+bot.addModule(new UpdatesModule(bot));
 
 bot.login();
 
